eccaa4e60093d83e0427e4a0101899b956a87222
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2024-11-29 22:30:12 +0200

    Updated User Validation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* -------------------------------- Variables ------------------------------- */
+/* ---------------------------- Global Variables ---------------------------- */
 const emailRegex = /^\w+@\w+\.\w+$/i;
 
 var button = document.querySelector("button");
@@ -17,6 +17,51 @@ for (let i = 0; i < inputs.length; i++) {
   });
 }
 
+// Email Validation
+var emailValid = false;
+var email = "";
+
+inputs[0].addEventListener("blur", function () {
+  email = inputs[0].value;
+
+  if (!checkMail(email)) {
+    errMSG.innerHTML = "email can't be in this format";
+    errMSG.classList.replace("d-none", "d-block");
+    emailValid = false;
+  } else {
+    if (errMSG.classList.contains("d-block")) {
+      errMSG.classList.replace("d-block", "d-none");
+    }
+    emailValid = true;
+  }
+});
+
+// Button Press Validation
+button.addEventListener("click", function () {
+  if (fieldsEmpty(inputs)) {
+    errMSG.innerHTML = "you must enter all fields";
+    errMSG.classList.replace("d-none", "d-block");
+  } else if (!emailValid) {
+    errMSG.innerHTML = "email can't be in this format";
+    errMSG.classList.replace("d-none", "d-block");
+  } else {
+    var user = {
+      email: inputs[0].value,
+      password: inputs[1].value,
+    };
+
+    if (isValidUser(user) === true) {
+      // Login
+
+      // Redirect Page
+      // window.location.replace("index.html");
+    } else {
+      errMSG.innerHTML = isValidUser(user);
+      errMSG.classList.replace("d-none", "d-block");
+    }
+  }
+});
+
 /* -------------------------------- Functions ------------------------------- */
 function fieldsEmpty(array) {
   for (let i = 0; i < array.length; i++) {
@@ -31,9 +76,15 @@ function checkMail(string) {
   return emailRegex.test(string);
 }
 
-function userExists(userEmail) {
-  if (localStorage.getItem(userEmail) == null) {
-    return false;
+function isValidUser(user) {
+  var users = JSON.parse(localStorage.getItem("users"));
+  for (let i = 0; i < users.length; i++) {
+    if (user.email == users[i].email) {
+      if (user.password == users[i].password) {
+        return true;
+      }
+      return "incorrect email or password";
+    }
   }
-  return true;
+  return "user doesn't exist";
 }
